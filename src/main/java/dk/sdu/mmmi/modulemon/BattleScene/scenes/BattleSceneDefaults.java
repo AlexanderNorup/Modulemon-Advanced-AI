d700da2d49ef4634b6b184864fb1ffb45b54ddf0
Alexander Nørup <me@alexandernorup.com>
2023-10-26 15:43:32 +0200

    Make text boxes bigger to hold "Monte-Carlo Tree Search". Maybe just MCTS?

--- src/main/java/dk/sdu/mmmi/modulemon/BattleScene/scenes/BattleSceneDefaults.java
+++ src/main/java/dk/sdu/mmmi/modulemon/BattleScene/scenes/BattleSceneDefaults.java
@@ -11,11 +11,11 @@ public class BattleSceneDefaults {
     public static Position enemyBasePosition(){ return new Position(800, 400);}
     public static Position enemyMonsterPosition(){return new Position(850, 450);}
     public static Position playerMonsterPosition () {return new Position(320, 100);}
-    public static Position enemyHealthBoxPosition(){ return new Position(480, 550);}
-    public static float enemyHealthBoxWidth(){return 300f;}
+    public static Position enemyHealthBoxPosition(){ return new Position(430, 550);}
+    public static float enemyHealthBoxWidth(){return 380f;}
     public static float enemyHealthBoxHeight(){return 100f;}
-    public static Position playerHealthBoxPosition(){ return new Position(100, 300);}
-    public static float playerHealthBoxWidth(){return 350f;}
+    public static Position playerHealthBoxPosition(){ return new Position(60, 300);}
+    public static float playerHealthBoxWidth(){return 400f;}
     public static float playerHealthBoxHeight(){return 100f;}
     public static Position actionBoxPosition(float gameWidth){return new Position(gameWidth - 300, 155); }
     public static float actionBoxWidth(){return 250f;}
