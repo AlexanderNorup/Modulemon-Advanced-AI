b03c5fed3fa2aa6c67cadf84a562f82d575a7cdc
Victor Andreas Boye <viboy20@student.sdu.dk>
2023-11-28 08:45:02 +0100

    Changed start min in bestchild to -1

--- src/main/java/dk/sdu/mmmi/modulemon/MCTSBattleAI/MCTSBattleAI.java
+++ src/main/java/dk/sdu/mmmi/modulemon/MCTSBattleAI/MCTSBattleAI.java
@@ -265,7 +265,7 @@ public class MCTSBattleAI implements IBattleAI {
 
     private Node bestChild(Node node, float exploration_coefficient) {
         Node bestChild = null;
-        var bestUCT = Float.MIN_NORMAL;
+        float bestUCT = -1f;
 
         for (Node child : node.getChildren()) {
             var uct = calculateUCT(child, exploration_coefficient);
